231ee2a16595bde1bdfda99a379a9a42144b11c5
melkaylemur <1tata922@mail.ru>
2017-07-09 22:42:34 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <string>
 #include <boost/asio.hpp>
 
-#include "client_test.h"
+#include "server.h"
 
 int main(int argc, char **argv) {
-    client_test cl;
-    cl.configure("127.0.0.1", "8084");
-    while (true){
-	    std::cout << "Input message: " << std::endl;
-	    std::string str;
-	    std::cin >> str;
-	    cl.add_query_message(str);
-    }
+    server sv;
+    sv.configure();
+    sv.start();
     boost::asio::io_service io;
     boost::asio::io_service::work wrkr(io);
     io.run();
